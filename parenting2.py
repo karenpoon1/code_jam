5d2c832198a154b35d62a33a2b75c9cefba95b1c
Karen Poon <poonyanlam@gmail.com>
2020-04-05 04:12:10 +0800

    parenting works

--- parenting2.py
+++ parenting2.py
@@ -27,14 +27,13 @@ def read_file():
 no_of_test, activity_cases, numbers = read_file()
 
 def algorithm(no_of_test, activity_cases, numbers):
-    # occupance can only be 0 or 1 or 2
-    occupance = 0
+
     for i in range(no_of_test):
         string = 'C'
-        orig_current_case = {v: k for v, k in enumerate(activity_cases[i])}
-        current_case = dict(sorted(orig_current_case.items(), key=lambda v:v[1]))
-        key_list = list(current_case.keys())
-        current_case = list(current_case.values())
+        orig_current_case = [[v, k] for v, k in enumerate(activity_cases[i])]
+        current_case = sorted(orig_current_case, key = lambda x: x[1])
+        key_list = [i[0] for i in current_case]
+        current_case = [i[1] for i in current_case]
         number_of_activities = numbers[i]
 
         c_start, c_end = map(int, current_case[0])
@@ -56,37 +55,31 @@ def algorithm(no_of_test, activity_cases, numbers):
             if start >= c_end and start >= j_end:
                 string = same_person(string)
                 if list(string)[-1] == 'C':
-                    c_start, c_end = start, end
+                    c_end = end
                 else: 
-                    j_start, j_end = start, end
+                    j_end = end
 
             elif start < c_end and start < j_end:
                 impossible = True
-                continue
+                # continue
 
             elif start < c_end and start >= j_end:
                 # j needs to finish, j can start new!, c_end unchanged
-                if j_end <= c_start:
-                    string = diff_person(string)
-                else:
-                    string = same_person(string)
-                j_start, j_end = start, end
+                string += 'J'
+                j_end = end
                 
             elif start >= c_end and start < j_end:
                 # c needs to finish, c can start new!, j_end unchanged
-                if c_end <= j_start:
-                    string = same_person(string)
-                else:
-                    string = diff_person(string)
-                c_start, c_end = start, end
+                string += 'C'
+                c_end = end
 
         if impossible:
             string = 'IMPOSSIBLE'
         else:
             string = string
-            # string_list = list(string)
-            # string = [x for _,x in sorted(zip(key_list,string_list))]
-            # string = ''.join(string)
+            string_list = list(string)
+            string = [x for _,x in sorted(zip(key_list,string_list))]
+            string = ''.join(string)
         print("Case #{}: {}".format(i+1, string))
             
 algorithm(no_of_test, activity_cases, numbers)
